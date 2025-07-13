8e5413402d2640743393eee83bd2f83e22ecd896
Conroy95 <leandervdneut@ziggo.nl>
2025-07-13 22:16:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,22 @@
-const CACHE_NAME = 'roadtrip-cache-v1';
-const FILES_TO_CACHE = [
-  '.',
-  'index.html',
-  'style.css',
-  'app.js',
-  'route.json',
-  'manifest.json',
-  'assets/icons/icon-192.png',
-  'assets/icons/icon-512.png',
-  'assets/wildwest-bg.jpg'
-];
-
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(FILES_TO_CACHE);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('roadtrip-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './app.js',
+        './routes.json',
+        './manifest.json'
+      ]);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', evt => {
-  if (evt.request.mode !== 'navigate') {
-    evt.respondWith(
-      caches.match(evt.request).then(response => {
-        return response || fetch(evt.request);
-      })
-    );
-    return;
-  }
-  evt.respondWith(
-    fetch(evt.request).catch(() => {
-      return caches.match('index.html');
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
