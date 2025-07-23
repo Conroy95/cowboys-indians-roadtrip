41643c36e9232ff603fbc30792b5d6c412f505d1
Conroy95 <leandervdneut@ziggo.nl>
2025-07-23 20:46:18 +0200

    Update dag.js

--- dag.js
+++ dag.js
@@ -44,6 +44,29 @@ const programmaPerDag = {
   20: ['Vertrek naar Amsterdam']
 };
 
+const listPerDag = {
+  1: ['Aankomst in Denver', 'Huurauto ophalen', 'Hotel inchecken', 'Dinner in downtown'],
+  2: ['Rit naar Cheyenne', 'Bezoek State Capitol', 'Lunch bij The Albany', 'Cowboy Boots Statues', 'Overnachting'],
+  3: ['Dagtocht naar Fort Laramie', 'Wandelen in Curt Gowdy State Park'],
+  4: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  5: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  6: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  7: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  8: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  9: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  10: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  11: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  12: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  13: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  14: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  15: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  16: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  17: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  18: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  19: ['Vertrek naar Hot Springs', 'Bezoek Mammoth Site', 'Avondwandeling'],
+  20: ['Vertrek naar Amsterdam']
+};
+
 // Simpel programma per dag
 function getProgramma(dag) {
   return programmaPerDag[dag] || ['Programma nog niet ingevoerd voor deze dag.'];
@@ -73,6 +96,10 @@ if (!dagNum || isNaN(dagNum)) {
         <h1>${dagText} - ${trip.plaats}</h1>
         <p><em>${datumText}</em></p>
         <img src="img/${trip.foto}" alt="${trip.plaats}" />
+        <h2>Info</h2>
+        <ul class="info-list">
+          ${programma.map(item => `<li>${item}</li>`).join('')}
+        </ul>
         <h2>Programma</h2>
         <ul class="programma-list">
           ${programma.map(item => `<li>${item}</li>`).join('')}
